75be1c0682845d9058d6fc635556d854294fdc07
Lucian Marcos <phyrox.vash512@gmail.com>
2025-01-14 06:04:28 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yeeko_abc_message_models',
-    version='0.1.0',
+    version='0.1.1',
     description='models and abstract classes for creating and sending instant messages',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
